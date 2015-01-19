d4d1221b06f7b88babc85ab9189cc59fa7688ca3
Karshan Sharma <karshan.sharma@gmail.com>
2015-01-19 08:11:24 -0800

    Initial work on member indexer

--- Main.hs
+++ Main.hs
@@ -3,12 +3,14 @@ module Main where
 
 import Debug.Trace
 
+import Control.Applicative ((<$>))
 import Control.Arrow ((>>>))
 import Control.Lens
 import Control.Lens.TH
 import Control.Monad ((>=>))
 import Data.Data (Data)
 import Data.Data.Lens (template)
+import Data.List (intercalate)
 import Data.Maybe (mapMaybe, catMaybes)
 import Data.Proxy
 
@@ -18,6 +20,7 @@ import Language.Java.Parser
 import Text.Parsec.Error (ParseError)
 import Text.Show.Pretty (ppShow)
 
+pprint :: (Show a) => a -> IO ()
 pprint = putStrLn . ppShow
 
 parse :: String -> Either ParseError CompilationUnit
@@ -53,6 +56,48 @@ catchWithGetMessage (_, catches, _) = any go catches
           go' exceptionVar (MethodCall (Name _ ((Ident _ e):(Ident _ methodName):_)) _) = e == exceptionVar && methodName == "getMessage"
           go' _ _ = False
 
+type ClassDecl' = ([Modifier], Ident, [TypeParam], Maybe RefType, [RefType], ClassBody)
+type MethodDecl' = ([Modifier], [TypeParam], (Maybe Type), Ident, [FormalParam], [ExceptionType], MethodBody)
+
+members :: ClassDecl -> [MemberDecl]
+members x = concat $ catMaybes [ (\x -> x ^. _6 ^. _ClassBody & mapMaybe (^? _MemberDecl)) <$> (x ^? _ClassDecl)
+                      , (\x -> x ^. _4 ^. _EnumBody ^. _2 & mapMaybe (^? _MemberDecl)) <$> (x ^? _EnumDecl)
+                      ]
+
+localClasses :: MethodDecl' -> [ClassDecl]
+localClasses methodDecl = maybe [] (\a -> mapMaybe (^? _LocalClass) $ a ^. _Block) ((\a -> a ^. _7 ^. _MethodBody) methodDecl)
+
+classDecls :: CompilationUnit -> [ClassDecl]
+classDecls cu = cu ^. _CompilationUnit ^. _3 & mapMaybe (^? _ClassTypeDecl)
+
+package :: CompilationUnit -> [Ident]
+package cu = (maybe [] (\x -> x ^. _PackageDecl ^. _Name ^. _2) $ cu ^. _CompilationUnit ^. _1)
+
+methods :: CompilationUnit -> [([Ident], MemberDecl)]
+methods cu = concatMap (methodsHelper $ package cu) (classDecls cu)
+
+methodsHelper :: [Ident] -> ClassDecl -> [([Ident], MemberDecl)]
+methodsHelper pak classDecl = (zip (repeat (pak ++ [classDeclName classDecl])) $ (members classDecl)) 
+    ++ (concatMap (methodsHelper (pak ++ [classDeclName classDecl])) (mapMaybe (^? _MemberClassDecl) (members classDecl)))
+    ++ (concatMap (\m -> concatMap (methodsHelper (pak ++ [m ^. _4])) $ localClasses m) $ mapMaybe (^? _MethodDecl) (members classDecl))
+
+classDeclName :: ClassDecl -> Ident
+classDeclName (ClassDecl _ x _ _ _ _) = x
+classDeclName (EnumDecl _ x _ _) = x
+
+memberName :: MemberDecl -> Ident
+memberName (FieldDecl _ _ (x:_)) = go $ x ^. _VarDecl ^. _1
+    where
+        go (VarId x) = x
+        go (VarDeclArray x) = go x
+memberName (MethodDecl _ _ _ x _ _ _) = x
+memberName (ConstructorDecl _ _ x _ _ _) = x
+memberName (MemberClassDecl x) = classDeclName x
+memberName (MemberInterfaceDecl (InterfaceDecl _ x _ _ _)) = x
+
 playground = do
     file <- readFile "test.java"
-    either print (pprint . h) $ parse file
+    either print (mapM_ putStrLn . map myShow . methods) $ parse file
+
+myShow :: ([Ident], MemberDecl) -> String
+myShow (name, member) = intercalate "." $ (map (\x -> x ^. _Ident ^. _2) name) ++ [memberName member ^. _Ident ^. _2]
--- test.java
+++ test.java
@@ -1,6 +1,10 @@
 public class test {
 
+  Int local_integer;
+  Int init_devl_block = new Int(7+8);
+
   public static void f(String s) {
+    int f_local_integer;
     try {
       fromXml(s);
     } catch (Exception f) {
