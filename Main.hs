c9f59513494d71393e8ca2d0b29744151856e943
Karshan Sharma <karshan.sharma@gmail.com>
2015-01-20 14:20:53 -0800

    implemented: Filter all functions that have an argument of type HTTPServletRequest

--- Main.hs
+++ Main.hs
@@ -13,12 +13,13 @@ import qualified Data.ByteString.Char8 as BS
 import Data.Data (Data)
 import Data.Data.Lens (template)
 import Data.List (intercalate)
-import Data.Maybe (mapMaybe, catMaybes)
+import Data.Maybe (isJust, mapMaybe, catMaybes)
 import Data.Proxy
 import Data.Either
 
 import Language.Java.Syntax
 import Language.Java.Parser
+import Language.Java.Pretty (Pretty, prettyPrint)
 
 import System.Environment (getArgs)
 import System.IO (hPutStrLn, stderr)
@@ -60,11 +61,35 @@ genIndex files = do
   mapM_ (hPutStrLn stderr) $ [f ++ ": " ++ (filter (/= '\n') a) | (f, Left a) <- results]
   writeFile "index" $ show [(f,a) | (f, Right a) <- results]
   
+readIndex :: IO [(FilePath, CompilationUnit)]
+readIndex = read <$> readFile "index"
+
 xssMain :: IO ()
 xssMain = do
-  fcus <- read <$> readFile "index"
+  fcus <- readIndex
   let cus = map snd fcus
-  print $ map handlerFuncs cus
+  mapM_ (putStrLn . showOne) $ filter isHTTPHandler $ concatMap methods cus
 
-handlerFuncs :: CompilationUnit -> [MemberDecl]
-handlerFuncs cu = cu ^.. template
+-- This is just the first level. This won't include nested methods, but will include methods inside nested classes
+-- Therefore information about the fully qualified name of the method is lost
+isHTTPHandler :: MemberDecl -> Bool
+isHTTPHandler m = maybe False go $ m ^? _MethodDecl
+    where
+        go = any (\x -> isType "HttpServletRequest" $ x ^. _FormalParam ^. _2) . (^. _5)
+
+-- Util
+-- TODO safeHead
+isType :: String -> Type -> Bool
+isType s t = maybe False go $ ((^? _RefType) >=> (^? _ClassRefType)) t
+    where
+        go = (== s) . identString . fst . head . (^. _ClassType)
+
+identString :: Ident -> String
+identString x = x ^. _Ident ^. _2
+
+showOne :: (Pretty a, Show a) => a -> String
+showOne x = (takeWhile (/= ' ') $ show x) ++ ": " ++ prettyPrint x
+
+-- TODO look for a lens operator that will make this cleaner. something like filter (^# _MethodDecl)
+methods :: CompilationUnit -> [MemberDecl]
+methods = filter (isJust . (^? _MethodDecl)) . (^.. template)
