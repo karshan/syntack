67b9549f27c241bf1aa27efe99145ce4fc2a9c26
Karshan Sharma <karshan.sharma@gmail.com>
2015-06-19 11:40:41 -0700

    Cleanup
     - moved posToZipper, upTillExp to Syntack.Zipper
     - generalized upTillExp. upTillExp == upTill (undefined :: Exp)

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,8 @@
 {-# LANGUAGE ViewPatterns #-}
-import           Control.Arrow ((&&&))
 import           Control.Exception (SomeException, try)
 import           Control.Lens (_1, _2, _Left, over)
 
-import           Data.Bool (bool)
 import           Data.Either (lefts, rights)
-import           Data.Generics.Zipper (Zipper, toZipper, getHole, up)
-import           Data.Generics.Aliases (mkQ)
-import           Data.Generics.Validation (zeverything, collectList, preorder)
-import           Data.Maybe (isJust)
 
 import           Language.Java.Syntax
 import           Language.Java.Parser.Util (parseFile)
@@ -16,9 +10,8 @@ import           Language.Java.Parser.Util (parseFile)
 import           System.Environment (getArgs)
 import           System.IO.Util (putStdErrLn)
 import           Text.Parsec (ParseError)
-import           Text.Parsec.Pos (sourceLine, sourceColumn)
 
-import           Syntack.Zipper (ZC, nameme)
+import           Syntack.Zipper (ZC, ppj)
 
 usage :: String
 usage = "./syntack file-containing-list-of-filenames"
@@ -33,19 +26,4 @@ run :: FilePath -> IO ()
 run fileOfFiles = do
     fs <- mapM parseFile . lines =<< readFile fileOfFiles
     mapM_ (putStdErrLn . show) $ lefts fs
-    nameme $ head $ rights fs
-
-upTillExp :: ZC -> Maybe ZC
-upTillExp z = bool (upTillExp =<< up z) (Just z) $ isJust $ (getHole :: ZC -> Maybe Exp) z
-
-posToZipper :: CompilationUnit -> Int -> Int -> Maybe ZC
-posToZipper cu line col = 
-    fmap snd $ foldl (\a e -> bool a (Just e) ((fst e) `better` (fmap fst a))) Nothing $
-        zeverything collectList (preorder (mkQ Nothing identPos)) (toZipper cu)
-    where
-        identPos :: Ident -> Maybe (Int, Int)
-        identPos (Ident p _) = fmap (sourceLine &&& sourceColumn) p
-        better :: (Int, Int) -> Maybe (Int, Int) -> Bool
-        better e Nothing = True
-        better (nl, nc) (Just (l, c)) =
-            (abs (nl - line), abs (nc - col)) < (abs (l - line), abs (c - col))
+    ppj $ head $ rights fs
--- src/Syntack/Zipper.hs
+++ src/Syntack/Zipper.hs
@@ -1,17 +1,17 @@
 module Syntack.Zipper where
 
+import           Control.Arrow ((&&&))
 import           Data.Bool (bool)
-import           Data.Generics.Zipper (Zipper, toZipper, fromZipper)
-import qualified Data.Generics.Zipper as Z (getHole, up, down, left, right)
-import           Data.Generics.Zipper.Util (getHole, up, down, left, right)
-import           Data.Maybe (isNothing, fromMaybe)
-import           Data.Proxy (Proxy)
-import           Data.Typeable (Typeable)
+import           Data.Generics.Aliases (mkQ)
+import           Data.Generics.Validation (zeverything, collectList, preorder)
+import           Data.Generics.Zipper (Zipper, toZipper, fromZipper, getHole, up, down, left, right, query)
+import           Data.Typeable (Typeable, typeOf)
 
 import           Language.Java.Syntax
 import           Language.Java.Pretty (Pretty, prettyPrint)
 
 import           Text.Show.Pretty (ppShow)
+import           Text.Parsec.Pos (sourceLine, sourceColumn)
 
 pps :: (Show a) => a -> IO ()
 pps = putStrLn . ppShow
@@ -19,10 +19,22 @@ pps = putStrLn . ppShow
 ppj :: (Pretty a) => a -> IO ()
 ppj = putStrLn . prettyPrint
 
-zix :: Int -> Zipper a -> Zipper a
-zix n = left . (!! n) . iterate down . down
+-- zix :: Int -> Zipper a -> Zipper a
+-- zix n = left . (!! n) . iterate down . down
 
 type ZC = Zipper CompilationUnit
 
-nameme :: CompilationUnit -> IO ()
-nameme = undefined
+upTill :: (Typeable a) => a -> ZC -> Maybe ZC
+upTill t z = bool (upTill t =<< up z) (Just z) $ query typeOf z == typeOf t
+
+posToZipper :: CompilationUnit -> Int -> Int -> Maybe ZC
+posToZipper cu line col =
+    fmap snd $ foldl (\a e -> bool a (Just e) ((fst e) `better` (fmap fst a))) Nothing $
+        zeverything collectList (preorder (mkQ Nothing identPos)) (toZipper cu)
+    where
+        identPos :: Ident -> Maybe (Int, Int)
+        identPos (Ident p _) = fmap (sourceLine &&& sourceColumn) p
+        better :: (Int, Int) -> Maybe (Int, Int) -> Bool
+        better e Nothing = True
+        better (nl, nc) (Just (l, c)) =
+            (abs (nl - line), abs (nc - col)) < (abs (l - line), abs (c - col))
